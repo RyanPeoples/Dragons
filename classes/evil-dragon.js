badb1f1612bc4277fbcc85f76a5819a34ae3afbc
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-02-08 14:16:15 -0800

    evil dragonssss

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,24 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+  constructor(name, color, evilDoings, nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;
+  }
+  dontInviteThemOverForDinner() {
+    let arr = this.evilDoings;
+    // for (let i = 0; i < arr.length; i++) {
+    //     console.log(`${this.name} likes to ${arr[i]}`);
+    // }
+
+    arr.forEach(evilDoing => {
+      console.log(`${this.name} will ${evilDoing}`);
+    });
+  }
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+  }
+}
+
+module.exports = EvilDragon;

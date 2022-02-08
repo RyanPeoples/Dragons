6c61e7a0343cce495db364f3ed6f36821d18d2ad
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-08 15:40:44 -0700

    dragons done

--- all-dragons.js
+++ all-dragons.js
@@ -1,14 +1,12 @@
 const FriendlyDragon = require('./classes/friendly-dragon')
 const EvilDragon = require('./classes/evil-dragon')
-const getDragons = require('./classes/dragon')
-
+const Dragon = require('./classes/dragon')
 const falkor = new FriendlyDragon('Falkor', 'white', ["save Atreyu from the swamp",
-  "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], '"Bastian"');
+  "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], 'Bastian');
 
 const smaug = new EvilDragon('Smaug', 'black', ["take over your mountain kingdom",
   "steal all your dwarven gold", "burn down your floating village"], "Dwarf King");
 const allDragons = Dragon.getDragons(falkor, smaug);
-
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 

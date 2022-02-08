0293deaee224780b7dbe8e917c4383974420d6e5
Ryan Peoples <82687773+RyanPeoples@users.noreply.github.com>
2022-02-08 13:46:25 -0700

    Add files via upload

---
+++ classes/dragon.js
@@ -0,0 +1,15 @@
+class Dragon {
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Dragon;
+} catch {
+  module.exports = null;
+}

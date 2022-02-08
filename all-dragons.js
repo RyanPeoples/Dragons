2dc658f7293b7cd283f82a5d877945a221369874
Ryan Peoples <peoplesjermaine@gmail.com>
2022-02-08 15:35:36 -0700

    working on all

--- all-dragons.js
+++ all-dragons.js
@@ -1,4 +1,13 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon')
+const EvilDragon = require('./classes/evil-dragon')
+const getDragons = require('./classes/dragon')
+
+const falkor = new FriendlyDragon('Falkor', 'white', ["save Atreyu from the swamp",
+  "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], '"Bastian"');
+
+const smaug = new EvilDragon('Smaug', 'black', ["take over your mountain kingdom",
+  "steal all your dwarven gold", "burn down your floating village"], "Dwarf King");
+const allDragons = Dragon.getDragons(falkor, smaug);
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
@@ -7,6 +16,7 @@ try {
   module.exports = {
     falkor,
     smaug,
+    allDragons,
   };
 } catch {
   module.exports = null;
